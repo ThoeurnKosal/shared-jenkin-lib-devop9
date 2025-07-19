de1cc8c5c7172052b9cb2a85218c942b40bc3861
Thoeurn Kosal <thoeurnkosalitc@gmail.com>
2025-07-19 11:19:44 +0700

    update code

--- vars/scanReactJsSonarqubeV2.groovy
+++ vars/scanReactJsSonarqubeV2.groovy
@@ -1,23 +1,19 @@
-def call(String projectName, String projectKey , String projectVersion){
-  
-         withSonarQubeEnv(credentialsId: 'SONARQUBE-TOKEN', installationName: 'sonarqube-server') {
+def call(String projectKey , String projectName, String projectVersion){
+
+withSonarQubeEnv(credentialsId: 'SONARQUBE_TOKEN',
+ installationName: 'sonarqube-scanner') {
                 script{
-                
-                //  def projectKey = 'reactjs-devops8-template' 
-                //     def projectName = 'Reactjs DevOps8 template'  // Replace with your project name
-                //     def projectVersion = '1.0.0'  // Replace with your project version
-                //             // sh "${scannerHome}/bin/sonar-scanner"
                     sh """
                     ${scannerHome}/bin/sonar-scanner \
                     -Dsonar.projectKey=${projectKey} \
                     -Dsonar.projectName="${projectName}" \
                     -Dsonar.projectVersion=${projectVersion} \
-                    -Dsonar.exclusions="pipes/**"
                      """   
                         
                         }
             }
-            // dir("pipes")
 
-    
+
+
+
 }
--- vars/sendTelegramMessageV2.groovy
+++ vars/sendTelegramMessageV2.groovy
@@ -1,8 +1,18 @@
-def call(String message, String token, String chatId) {
+def call(String message) {
+                
+    withCredentials([usernamePassword(credentialsId: 'TELEGRAM_BOT',
+    passwordVariable: 'TOKEN', usernameVariable: 'CHAT_ID')]) {
+      def encodedMessage = URLEncoder.encode(message, "UTF-8")
     sh """
-    curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
-    -d chat_id=${chatId} \
-    -d text="${message}" \
-    -d parse_mode=MarkdownV2
+        curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage \\
+        -d chat_id=${CHAT_ID} \\
+        -d text="${encodedMessage}" > /dev/null
     """
+
+           
+        
+        
+    }     
+
+    
 }
